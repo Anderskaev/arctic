70328394cf10aa4c02d80872c29c61a5e30ed9ab
Vlad <v.anderskaev@gmail.com>
2026-04-20 17:51:01 +0300

    Fix navbar

--- frontend/meet-arctic-front/src/components/hero.js
+++ frontend/meet-arctic-front/src/components/hero.js
@@ -6,7 +6,7 @@ export default function Hero() {
             <div className="hero-tag">66°33′44″ N — Polar circle</div>
             <h1>Meet the Arctic</h1>
             <p>Explore the Arctic - place where people live despite the harsh natural conditions.</p>
-            <Link to="/cardlist">
+            <Link to="/cardlist"  style={{ textDecoration: 'none' }}>
             <button className="btn btn-ghost">
                 View settlements
             </button>
--- frontend/meet-arctic-front/src/components/navbar.js
+++ frontend/meet-arctic-front/src/components/navbar.js
@@ -4,28 +4,29 @@ import { NavLink } from 'react-router'
 export default function Navbar() {
   return (
     <>
-    <div className='nav'>
-       <div className="nav-logo">
-        <div className="nav-logo-icon">
-          <svg viewBox="0 0 24 24"><path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path></svg>
+      <div className='nav'>
+        <div className="nav-logo">
+          <div className="nav-logo-icon">
+            <svg viewBox="0 0 24 24"><path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path></svg>
+          </div>
+          Meet the Arctic
+
         </div>
-        Meet the Arctic
-      </div> 
-    </div>
-      <div className='tabs'>
-        <NavLink 
-          to="/" 
-          className={({ isActive }) => isActive ? 'tab active' : 'tab'}
-          style={{ textDecoration: 'none' }}>
-        <div>Главная</div>
-        </NavLink >
-        <NavLink  
-          to="/cardlist" 
-          className={({ isActive }) => isActive ? 'tab active' : 'tab'}
-          style={{ textDecoration: 'none' }}>
-          <div>Список городов</div>
-        </NavLink >
+        <div className='tabs'>
+          <NavLink
+            to="/"
+            className={({ isActive }) => isActive ? 'tab active' : 'tab'}
+            style={{ textDecoration: 'none' }}>
+            <div>Home</div>
+          </NavLink >
+          <NavLink
+            to="/cardlist"
+            className={({ isActive }) => isActive ? 'tab active' : 'tab'}
+            style={{ textDecoration: 'none' }}>
+            <div>View settlements</div>
+          </NavLink >
+        </div>
       </div>
-      </>    
+    </>
   )
 }
